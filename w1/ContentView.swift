ad4f0ca2a0ebfa9de796ed19face14731e85be13
alsmhahmed <alsmh.ahmed@gen-c.com>
2024-09-26 17:02:18 +0300

    Buttons, cardCountAdjusters, cards

--- w1/ContentView.swift
+++ w1/ContentView.swift
@@ -8,39 +8,79 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis: [String] = ["👻","🎃","🕷️","😈","😈"]
+    let emojis: [String] = ["👻","🎃","🕷️","😈","👽","🤡","🤠","👨🏻‍🚀","🌝"]
+    @State var cardCount: Int = 4
+    
     var body: some View {
+        VStack{
+            cards
+            cardCountAdjusters
+        }
+        .padding()
+    }
+    
+    var cards: some View {
         HStack{
-            ForEach(emojis.indices, id: \.self) { index in
+            ForEach(0..<cardCount, id: \.self) { index in
                 CardView(content: emojis[index])
             }
         }
-        .padding()
         .foregroundStyle(.orange)
     }
-}
-
-
-struct CardView: View {
-    let content: String
-    @State var isFaceUp = true
+    
+    var cardCountAdjusters: some View {
+        HStack{
+            cardAdder
+            Spacer()
+            cardRemover
+        }
+        .imageScale(.large)
+        .font(.title3)
+        .padding(.horizontal, 4)
+    }
     
     
-    var body: some View {
-        ZStack {
-            let base = RoundedRectangle(cornerRadius: 12)
-            if isFaceUp {
-                base.fill(.white)
-                base.strokeBorder(lineWidth: 2.0)
-                Text(content).font(.largeTitle)
-            } else {
-                base.fill()
+    var cardRemover: some View {
+        Button(action: {
+            if cardCount > 1 {
+                cardCount -= 1
             }
-        }
-        .onTapGesture {
-            isFaceUp.toggle()
-        }
+        }, label: {
+            Image(systemName: "rectangle.stack.badge.minus.fill")
+        })
+    }
+    var cardAdder: some View {
+        Button(action: {
+            if cardCount < emojis.count {
+                cardCount += 1
+            }
+        }, label: {
+            Image(systemName: "rectangle.stack.badge.plus.fill")
+        })
+    }
+    
+    
+    struct CardView: View {
+        let content: String
+        @State var isFaceUp = true
         
+        
+        var body: some View {
+            ZStack {
+                let base = RoundedRectangle(cornerRadius: 12)
+                if isFaceUp {
+                    base.fill(.white)
+                    base.strokeBorder(lineWidth: 2.0)
+                    Text(content).font(.largeTitle)
+                } else {
+                    base.fill()
+                }
+            }
+            .onTapGesture {
+                isFaceUp.toggle()
+            }
+            
+        }
     }
 }
 
